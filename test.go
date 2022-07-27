10cf8a7161122335c13dd8fcd6b188a6b4bce5b9
devvspaces <netrobepy@gmail.com>
2022-07-27 12:06:34 +0100

    learnt structs, interfaces, has-is, is-a relationships Learnt goroutines, channels and context Built my own sleep function

--- test.go
+++ test.go
@@ -1,36 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
+	"time"
 )
 
+func Sleep(d time.Duration) {
+	func() {
+		select {
+		case <-time.After(d):
+		}
+	}()
+}
+
 func main() {
-	defer func() {
-		str := recover()
-		if str != nil {
-			fmt.Println("Make sure you passed a file name")
+	c1 := make(chan string)
+	c2 := make(chan string)
+	c3 := make(chan bool)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		Sleep(time.Second * 5)
+		cancel()
+	}()
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			Sleep(time.Second * 2)
 		}
 	}()
 
-	allowed_flags := []string{
-		"-f",
-	}
+	go func() {
+		for {
+			c2 <- "from 2"
+			Sleep(time.Second * 3)
+		}
+	}()
 
-	if len(os.Args) > 1 {
-		flag := os.Args[1]
-		found := false
-		for _, value := range allowed_flags {
-			if flag == value {
-				found = true
-				file_name := os.Args[2]
-				fmt.Println(file_name)
-				break
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			case <-ctx.Done():
+				fmt.Println("Time's up")
+				c3 <- true
+				return
 			}
 		}
-		if found == false {
-			panic("Invalid Flag used")
-		}
-	}
+	}()
 
+	<-c3
+	fmt.Println("Finished")
 }
